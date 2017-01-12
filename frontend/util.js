c5480bba13a463bd23f8f1fe3e83bdc10e879da9
zhanwentao <zhanwentao@baijiahulian.com>
2017-01-12 16:56:25 +0800

    上传到github

---
+++ frontend/util.js
@@ -0,0 +1,23 @@
+define(function (require) {
+    var fn = {};
+    fn.Date = {
+        /**
+         * 返回一个月的第一天
+         * @param  {Date} date Date格式的时间
+         * @return {Date}      处理后的时间
+         */
+        firstDayInMonth: function (date) {
+
+        },
+        /**
+         * 返回一个月的最后一天
+         * @param  {Date} date Date格式的时间
+         * @return {Date}      处理后的时间
+         */
+        lastDayInMonth: function (date) {
+
+        }
+    }
+
+    return fn;
+});
